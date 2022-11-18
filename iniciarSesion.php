c3c5420d59e076847055275e6ff77928f6a7f0ed
unknown <Administrador@D129741.uao.edu.co>
2022-11-18 12:30:49 -0500

    Catching password and username and finishing log in

--- iniciarSesion.php
+++ iniciarSesion.php
@@ -1,19 +1,22 @@
 <?php
     include 'conexion.php';
 
-    $data = array("a" => "Apple", "b" => "Ball", "c" => "Cat");
+    $usuario = $_GET["usuario"];
+    $contrasena = $_GET["contrasena"];
 
-    $sentencia = $conexion -> prepare("SELECT * FROM escobar_usuarios");
+    $sentencia = $conexion -> prepare("SELECT * FROM escobar_usuarios WHERE usuario = ? AND contrasena = ?");
+    $sentencia -> bind_param('ss', $usuario, $contrasena);
     $sentencia -> execute();
+
+    header("Content-Type: application/json");
 
     $resultado = $sentencia -> get_result();
     if ($fila = $resultado -> fetch_assoc()) {
         echo json_encode($fila, JSON_UNESCAPED_UNICODE);
+    } else {
+        echo json_encode(false);
     }
 
     $sentencia -> close();
     $conexion -> close();
-
-    header("Content-Type: application/json");
-    echo json_encode($data);
 ?>
